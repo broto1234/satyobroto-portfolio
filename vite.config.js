05c26b8edbb09982083f7d74a6a554efd59cc428
broto1234 <satyo97du@gmail.com>
2025-09-07 22:45:40 +0200

    added new

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ import tailwindcss from '@tailwindcss/vite'
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 // import tailwindcss from '@tailwindcss/vite'
-// import path from 'path'
+import path from 'path'
 
 // // https://vite.dev/config/
     export default defineConfig({
@@ -22,4 +22,5 @@ import tailwindcss from '@tailwindcss/vite'
           "@": path.resolve(__dirname, './src'),
         },
       },
+      base: '/satyobroto-portfolio/',
     });
