d1c3488763c98c440af0edd9a2af686f7c32e467
broto1234 <satyo97du@gmail.com>
2025-09-08 11:20:32 +0200

    added things

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  css: {
+    devSourceMap: true
+  },
   build: {
     outDir: 'docs',
   },
