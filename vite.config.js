1dcf76090ef276cd64df97d5ccc6e94798a92a48
broto1234 <satyo97du@gmail.com>
2025-09-08 09:12:55 +0200

    Added somethings

--- vite.config.js
+++ vite.config.js
@@ -2,25 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-//   base: '/satyobroto-portfolio/',
-// })
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  base: '/satyobroto-portfolio/',
+})
 
 
 
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 // import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+// import path from 'path'
 
 // // https://vite.dev/config/
-    export default defineConfig({
-      plugins: [react(), tailwindcss()],
-      resolve: {
-        alias: {
-          "@": path.resolve(__dirname, './src'),
-        },
-      },
-      base: '/satyobroto-portfolio/',
-    });
+    // export default defineConfig({
+    //   plugins: [react(), tailwindcss()],
+    //   // resolve: {
+    //   //   alias: {
+    //   //     "@": path.resolve(__dirname, './src'),
+    //   //   },
+    //   // },
+    //   base: '/satyobroto-portfolio/',
+    // });
